cd71dceabadbcaee8baa4a6fd3a16ba108aa0f32
amondalgit <adarsha.mondal55@gmail.com>
2023-03-05 16:01:13 +0530

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+
+    with open(file_path) as file_obj:
+        requirements = [req.strip() for req in file_obj.readlines()]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
+
+setup(
+name = 'mlproject',
+version = '0.0.1',
+author = 'amondal',
+author_email = 'adarsha.mondal55@gmail.com',
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
+)
